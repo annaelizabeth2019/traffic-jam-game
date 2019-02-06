ecb709f5f1e038952f32bf8eb9d75e9d93fb5335
Anna Peterson <annapeterson@Annas-MacBook-Pro.local>
2019-02-06 12:19:09 -0600

    Added logic for cars to be draggable

--- js/car.js
+++ js/car.js
@@ -14,3 +14,130 @@
 //     }
 
 // };
+
+
+var canvas, cx, width, height;
+var canvasRect;
+
+var cube1, cube2;
+
+var dragData = {
+    draggables: [],
+    start: {        x: 0,        y: 0
+    },
+    current: {      x: 0,        y: 0
+    },
+    target: null
+};
+
+function Cube(x,y,w,h, color) {
+  this.x=x; this.y=y; this.w=w; this.h = h;
+  this.color = color;
+}
+
+Cube.prototype = {
+    update: function () {
+
+    },
+    draw: function (ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    },
+    isPointInside: function (x, y) {
+        return (x >= this.x) && (x < this.x + this.w) && (y > this.y) && (y < this.y + this.h);
+    }
+};
+
+var pointerCoords = {
+    x: 0,
+    y: 0,
+    update: function (e) {
+        var coords = e.touches ? e.touches[0] : e;
+        this.x = coords.pageX - canvasRect.left;
+        this.y = coords.pageY - canvasRect.top;
+    }
+};
+
+
+function onStart(e) {
+    e.preventDefault();
+    pointerCoords.update(e);
+    // look if we start the touch within a draggable object
+    var target = null;
+    for (var i = 0; i < dragData.draggables.length; i++) {
+        var draggable = dragData.draggables[i];
+        if (draggable.isPointInside(pointerCoords.x, pointerCoords.y)) {
+            target = draggable;
+            break;
+        }
+    }
+    dragData.target = target;
+}
+
+function onMove(e) {
+    pointerCoords.update(e);
+    var target = dragData.target;
+    if (!target) return;
+    target.x = pointerCoords.x;
+    target.y = pointerCoords.y;
+}
+
+function onStop(e) {
+    pointerCoords.update(e);
+    e.preventDefault();
+    if (!dragData.target) return;
+    onMove(e);
+    dragData.target = null;
+}
+
+function main() {
+    canvas = document.createElement("canvas");
+
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    if (width >= 500) {
+        width = 320;
+        height = 480;
+        canvas.style.border = "1px solid #000";
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    ctx = canvas.getContext("2d");
+    document.body.appendChild(canvas);
+    canvasRect = canvas.getBoundingClientRect();
+    canvas.addEventListener("touchstart", onStart);
+    canvas.addEventListener('touchmove', onMove);
+    canvas.addEventListener("touchstop", onStop);
+    canvas.addEventListener("mousedown", onStart);
+    canvas.addEventListener('mousemove', onMove);
+    canvas.addEventListener("mouseup", onStop);
+    
+    cube1 = new Cube(100, 80, 100, 200, 'blue');
+    cube2 = new Cube(150, 160, 200, 100, 'red');
+    dragData.draggables.push(cube1, cube2);
+    run();
+}
+
+
+function run() {
+    var loop = function () {
+        requestAnimationFrame(loop);
+        update();
+        render();
+    }
+    loop();
+}
+
+function update() {
+
+}
+
+function render() {
+    ctx.clearRect(0, 0, width, height);
+    cube1.draw(ctx);
+    cube2.draw(ctx);
+}
+
+main();
--- js/main.js
+++ js/main.js
@@ -5,22 +5,18 @@
     //call requestAnimationFrame inside of init()
     //call requestAnimationFrame 2x: in init to know the origin points, and when on a loop. 
 //Traffic Jam
-
 //2. Blocks can be move on x and y axis
     //figure out where the mouse is and how far it has traveled
     //How to get area of cars?
     //if mouse's x is > car's x + width (ish?)
 //3. Blocks can be dragged by mouse 
     // figure out and know when the mouse is inside of a car
-    //know when 
     //
 //4. The game is winnable: player can get the red car to the escape point and know they won
 //5. The game runs a timer?
 //6. The game counts moves. 
 //7. The game includes at least one level, but potentially can include more levels that can be easily rendered
 //8. The game lets the player know when they have won!
-
-// ctx.clearRect(0, 0, 600, 600)
 
 
 /*----- constants -----*/ 
@@ -32,127 +28,154 @@ var fctx = frontCanvas.getContext("2d");
 const CANVAS_WIDTH = frontCanvas.width;
 const CANVAS_HEIGHT = frontCanvas.height;
 
+/*----- app's state (variables) -----*/ 
+
 //dragging detection
 let dragging = false;
-let x, y;
-let carY;
+let mouseX, mouseY;
 
 let redCar, car1, car2, car3, car4, car5, car6, car7, car8, car9, bus1, bus2;
 
+let cars = [];
+
 class Car {
-    constructor(carX, originY, carWidth, carHeight){
+    constructor(originX, originY, carWidth, carHeight, orientation){
         this.width = carWidth;
         this.height = carHeight;
-        this.x = carX;
+        this.x = originX;
         this.y = originY;
         this.padding = 10;
+        this.isSelected = false;
+        this.position = orientation;
     }
     
     draw(){
-        fctx.fillStyle = "rgb(58, 135, 154)";
+        //saves the current state of the canvas code
+        fctx.save();
+        //clear
+        //draw
+        fctx.fillStyle = "violet";
         fctx.fillRect(this.x + this.padding, this.y + this.padding, this.width, this.height);
+        fctx.strokeStyle = "black";
+        fctx.strokeRect(this.x + this.padding, this.y + this.padding, this.width, this.height);        
+        //restores
+        fctx.restore();
         
-    }
-    clickEvent () {
+    // if(dragging){
+        //     x=mouseX-size/2;
+        //     y=mouseY-size/2;
+        // }
+            
+    // function doMouseMove(event){
+    //     mouseX = event.pageX-canvas.offsetLeft;
+    //     mouseY = event.pageY-canvas.offsetTop;
+    //     console.log("x:"+mouseX+", y:"+mouseY);
+    // }
+                
+            }
+            clickEvent () {
+                
+            }
+            
+            //do I need update() in car class? (I think I do)
+        };
         
-        console.log(this.x)
-    }
-    
-};
-
 function init(){
-
-    createGrid();
-
-    createCars();
-
+            
     /*----- event listeners -----*/ 
-    
+            
     frontCanvas.addEventListener('mousedown', selectCar)
     frontCanvas.addEventListener('mousemove', mouseTrack);
-    frontCanvas.addEventListener('mouseup', releaseCar);
-
-
-
-Car.prototype.moveX = function() {
-    if (dragging === true) {
-        this.x = carX
-    }
+    document.addEventListener('mouseup', releaseCar);
+            
+    /*---- make the game board and cars ----*/
+    createGrid();
+    createCars(); 
+    
+    /*--- Starts the update loop ----*/
+    
+    requestAnimationFrame(update);
+    
 };
-};
-
-/*----- app's state (variables) -----*/ 
-
-let carX = 100; 
-let dx = 100; 
 
 
 /*----- cached element references -----*/ 
 
 
 
-
-
-
 /*----- functions -----*/
 
 function selectCar() {
-    console.log(frontCanvas.style.right);
-    // let temp = frontCanvas.style.right.split('p')
-    // console.log(temp)
-    
-    // let num = Number(temp[0]) + 100;
-    // temp[0] = num.toString();   
-    
-    // temp = temp.join('p')
-    
-    // frontCanvas.style.right = temp;
-}
+    dragging = true;
+    for (let i = 0; i < cars.length; i++){
+        var c = cars[i];
+        if (mouseX >= c.x && mouseX < c.x + c.width && mouseY >= c.y && mouseY < c.y + c.height){
+            c.isSelected = true;
+        } else {
+            c.isSelected = false;
+        }
+    }
+};
 
 function mouseTrack(evt) {
     let rect = frontCanvas.getBoundingClientRect();
-    x = evt.clientX - rect.left;
-    y = evt.clientY - rect.top;
-    if (dragging === true) {
-        
-    }
+    mouseX = evt.clientX - rect.left;
+    mouseY = evt.clientY - rect.top;   
     
+    for (let i = 0; i < cars.length; i++){
+        var c = cars[i];
+        if (dragging === true && c.isSelected && c.position === 'h') {
+            c.x = mouseX - c.width/2;
+        } else if (dragging === true && c.isSelected && c.position === 'v'){
+            c.y = mouseY - c.height/2;
+        }
+    }
 };
 
 function releaseCar(){
-    console.log('release car is ready!');
     dragging = false;
+    for (let i = 0; i < cars.length; i++){
+        var c = cars[i];
+        c.isSelected= false; 
+    }
 };
 
 function render() {
-    console.log('render is ready');
+    // console.log('render is ready');
 };
 
 function animate() {
     requestAnimationFrame(animate);
-    carX += dx;
-    console.log(carX);
+    if (x + 90 > CANVAS_WIDTH || x < 0) {
+        dx = 0;
+    }
+    // mouseX += dx;
+    update();
+    
 };
 
 function createCars() {
-    // cars
-    redCar = new Car(0, 200, 180, 80);
-    car1 = new Car(0, 300, 180, 80);
-    car2 = new Car(100, 400, 80, 180);
-    car3 = new Car(0, 400, 80, 180);
-    car4 = new Car(200, 400, 180, 80);
-    car5 = new Car(200, 500, 180, 80);
-    car6 = new Car(200, 200, 80, 180);
-    car7 = new Car(200, 0, 80, 180);
-    car8 = new Car(300, 100, 80, 180);
-    car9 = new Car(300, 300, 180, 80);
-    bus1 = new Car(300, 0, 280, 80);
-    bus2 = new Car(500, 200, 80, 280);
+    // cars and originX, origin Y, height, width
+    cars = [
+        redCar = new Car(0, 200, 180, 80, 'h'),
+        car1 = new Car(0, 300, 180, 80, 'h'),
+        car2 = new Car(100, 400, 80, 180, 'v'),
+        car3 = new Car(0, 400, 80, 180, 'v'),
+        car4 = new Car(200, 400, 180, 80, 'h'),
+        car5 = new Car(200, 500, 180, 80, 'h'),
+        car6 = new Car(200, 200, 80, 180, 'v'),
+        car7 = new Car(200, 0, 80, 180, 'v'),
+        car8 = new Car(300, 100, 80, 180, 'v'),
+        car9 = new Car(300, 300, 180, 80, 'h'),
+        bus1 = new Car(300, 0, 280, 80, 'h'),
+        bus2 = new Car(500, 200, 80, 280, 'v')
+    ];
+
 };
 
 function createGrid() {
     ctx.strokeRect(0, 0, 600, 600);
-
+    
     //rows
     ctx.strokeStyle = "pink";
     ctx.lineWidth = 1;
@@ -168,7 +191,7 @@ function createGrid() {
     ctx.moveTo(600, 500);
     ctx.lineTo(0, 500)
     ctx.stroke();
-
+    
     //columns
     ctx.beginPath();
     ctx.moveTo(100, 0);
@@ -187,22 +210,37 @@ function createGrid() {
 };
 
 function update() {
-    redCar.draw();
-    car1.draw();
-    car2.draw();
-    car3.draw();
-    car4.draw();
-    car5.draw();
-    car6.draw();
-    car7.draw();
-    car8.draw();
-    car9.draw();
-    bus1.draw();
-    bus2.draw();
-    update();
-    };
-    // requestAnimationFrame();
+    fctx.clearRect(0, 0, 600, 600)
 
+    for (i = 0; i < cars.length; i++){
+        cars[i].draw()
+    };
+
+    // redCar.draw();
+    // car1.draw();
+    // car2.draw();
+    // car3.draw();
+    // car4.draw();
+    // car5.draw();
+    // car6.draw();
+    // car7.draw();
+    // car8.draw();
+    // car9.draw();
+    // bus1.draw();
+    // bus2.draw();
+    requestAnimationFrame(update);
+
+//some logic for collision detection
+    // if (this.position.x < 0) {
+    //     this.position = 0;
+    // };
+    // if (this.position.x + this.width > CANVAS_WIDTH) {
+    //     this.position = 0;
+    // }
+
+    // };
+    // requestAnimationFrame();
+};
 
 // animate()
 
